2f4f6ec9cfc2fe3998245c397d210445c0d093b3
Adrian Opsahl <adrian.opsahl@gmail.com>
2023-09-21 22:00:23 +0200

    base template and db

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
 # ![RealWorld Example App](logo.png)
 
-> ### [YOUR_FRAMEWORK] codebase containing real world examples (CRUD, auth, advanced patterns, etc) that adheres to the [RealWorld](https://github.com/gothinkster/realworld) spec and API.
+> ### Go with PocketBase and htmx codebase containing real world examples (CRUD, auth, advanced patterns, etc) that adheres to the [RealWorld](https://github.com/gothinkster/realworld) spec and API.
 
 
 ### [Demo](https://demo.realworld.io/)&nbsp;&nbsp;&nbsp;&nbsp;[RealWorld](https://github.com/gothinkster/realworld)
 
 
-This codebase was created to demonstrate a fully fledged fullstack application built with **[YOUR_FRAMEWORK]** including CRUD operations, authentication, routing, pagination, and more.
-
-We've gone to great lengths to adhere to the **[YOUR_FRAMEWORK]** community styleguides & best practices.
+This codebase was created to demonstrate a fully fledged fullstack application built with [PocketBase](https://pocketbase.io) and [htmx](https://htmx.org) including CRUD operations, authentication, routing, pagination, and more.
 
 For more information on how to this works with other frontends/backends, head over to the [RealWorld](https://github.com/gothinkster/realworld) repo.
 
 
 # How it works
+The motivation behind this project was to make a very simple, yet powerful way of prototyping solutions. Go is a very simple language, SQLite is a simple DB and htmx is a simple way of making reactive frontends. Pocketbase is here to make it easier to use certain features available and to change the data model in an easier way.
 
-> Describe the general architecture of your app here
+The hope is to build a simple prototype that is self-contained, trivial to host anywhere and powerful enough for most use cases. It is also possible to write everything in Javascript and to use one of the popular SPA-frameworks, but that is for another time...
 
 # Getting started
+```sh
+go mod tidy
+go run main.go serve
+```
 
-> npm install, npm start, etc.
-
+## Login to PocketBase
+You can use user: `admin@realworld.com` and password: `adminadmin` in the provided db.
---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/template"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		registry := template.NewRegistry()
+
+		e.Router.GET("/hx/tags", func(c echo.Context) error {
+			records, err := app.Dao().FindRecordsByExpr("tags")
+			if err != nil {
+				return err
+			}
+			tags := make([]string, 0, len(records))
+			for _, r := range records {
+				tags = append(tags, r.GetString("tag"))
+			}
+
+			html, err := registry.LoadFiles(
+				"components/tags.html",
+			).Render(tags)
+			if err != nil {
+				return err
+			}
+
+			return c.HTML(http.StatusOK, html)
+		}, apis.ActivityLogger(app))
+
+		return nil
+	})
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		registry := template.NewRegistry()
+
+		e.Router.GET("/", func(c echo.Context) error {
+			html, err := registry.LoadFiles(
+				"views/layout.html",
+				"views/home.html",
+			).Render(nil)
+
+			if err != nil {
+				return apis.NewNotFoundError("", err)
+			}
+
+			return c.HTML(http.StatusOK, html)
+		})
+		e.Router.GET("/login", func(c echo.Context) error {
+			html, err := registry.LoadFiles(
+				"views/layout.html",
+				"views/login.html",
+			).Render(nil)
+
+			if err != nil {
+				return apis.NewNotFoundError("", err)
+			}
+
+			return c.HTML(http.StatusOK, html)
+		})
+		e.Router.GET("/register", func(c echo.Context) error {
+			html, err := registry.LoadFiles(
+				"views/layout.html",
+				"views/register.html",
+			).Render(nil)
+
+			if err != nil {
+				return apis.NewNotFoundError("", err)
+			}
+
+			return c.HTML(http.StatusOK, html)
+		})
+		e.Router.GET("/settings", func(c echo.Context) error {
+			html, err := registry.LoadFiles(
+				"views/layout.html",
+				"views/settings.html",
+			).Render(nil)
+
+			if err != nil {
+				return apis.NewNotFoundError("", err)
+			}
+
+			return c.HTML(http.StatusOK, html)
+		})
+		e.Router.GET("/article/:slug", func(c echo.Context) error {
+			html, err := registry.LoadFiles(
+				"views/layout.html",
+				"views/register.html",
+			).Render(nil)
+
+			if err != nil {
+				return apis.NewNotFoundError("", err)
+			}
+
+			return c.HTML(http.StatusOK, html)
+		})
+
+		return nil
+	})
+
+	// serves static files from the provided public dir (if exists)
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
